03ba1be11e52482669d1ddcc7bdf6b05db03682a
Jakub Dyszkiewicz <jakub.dyszkiewicz@allegro.pl>
2018-12-03 20:48:07 +0100

    Day 3

---
+++ day3/slices/src/main/scala/Slices.scala
@@ -0,0 +1,50 @@
+import scala.io.Source
+
+object Slices {
+
+  implicit class Crossable[X](xs: Traversable[X]) {
+    def cross[Y](ys: Traversable[Y]): Traversable[(X, Y)] = for {x <- xs; y <- ys } yield (x, y)
+  }
+
+  case class Claim(id: Int, startX: Int, startY: Int, width: Int, height: Int) {
+    def size: Int = width * height
+  }
+
+  type Coordinates = (Int, Int)
+
+  class Area(val fields: Map[Coordinates, Set[Claim]] = Map()) {
+
+    def claim(claim: Claim): Area = {
+      val claims = Range(claim.startX, claim.startX + claim.width)
+        .cross(Range(claim.startY, claim.startY + claim.height))
+        .map(coords => coords -> (fields.getOrElse(coords, Set()) + claim))
+        .toMap
+      new Area(fields ++ claims)
+    }
+
+    def multipleClaimFields: Map[Coordinates, Set[Claim]] =
+      fields.filter { case(_, claims) => claims.size > 1 }
+
+    def idOfNotClaimedByAnyoneElse: Option[Int] =
+      fields.filter { case(_, claims) => claims.size == 1 }
+        .groupBy { case(_, claims) => claims.head }
+        .find { case(coords, claims) => coords.size == claims.size }
+        .map { case(coords, _) => coords.id }
+
+  }
+
+  private val regex = """#(\d{1,}) @ (\d{1,}),(\d{1,}): (\d{1,})x(\d{1,})+""".r
+  def parseClaim(line: String): Claim = line match {
+    case regex(id, startX, startY, width, height) =>
+      Claim(id.toInt, startX.toInt, startY.toInt, width.toInt, height.toInt)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val claims = Source.fromResource("inputDay3.txt").getLines
+      .map(parseClaim)
+      .toList
+    val area = claims.foldLeft(new Area()) { (area, claim) => area.claim(claim) }
+    println(area.multipleClaimFields.size)
+    println(area.idOfNotClaimedByAnyoneElse)
+  }
+}
